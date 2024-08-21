88c91282d2954139cee4112bb20421676c6601c9
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2024-08-21 18:47:10 +0800

    🔧 Changed to production db

--- database.py
+++ database.py
@@ -6,14 +6,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Production
-# load_dotenv(dotenv_path=".env.local")
-# DATABASE_URL = str(os.getenv("POSTGRES_URL"))
-# engine = create_engine(DATABASE_URL)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# Base = declarative_base()
-
-# Dev Mode
-URL_DATABASE = "sqlite:///database.db"
-engine = create_engine(URL_DATABASE, connect_args={"check_same_thread": False})
+load_dotenv(dotenv_path=".env.local")
+DATABASE_URL = str(os.getenv("POSTGRES_URL"))
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+# Dev Mode
+# URL_DATABASE = "sqlite:///database.db"
+# engine = create_engine(URL_DATABASE, connect_args={"check_same_thread": False})
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
